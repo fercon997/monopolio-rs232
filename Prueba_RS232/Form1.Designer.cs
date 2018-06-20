a4a38f6a90b0b8be6f0a6a8574d4106c73bd8d25
Fernando Consalvo <fercon997@gmail.com>
2018-06-20 14:33:09 -0400

    Comunicación entre puertos RS-232

---
+++ Prueba_RS232/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Prueba_RS232
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetSerialPorts = new System.Windows.Forms.Button();
+            this.cboPorts = new System.Windows.Forms.ComboBox();
+            this.cboBaudRate = new System.Windows.Forms.ComboBox();
+            this.cboStopBits = new System.Windows.Forms.ComboBox();
+            this.cboHandShaking = new System.Windows.Forms.ComboBox();
+            this.lblBreakStatus = new System.Windows.Forms.Label();
+            this.lblCTSStatus = new System.Windows.Forms.Label();
+            this.lblDSRStatus = new System.Windows.Forms.Label();
+            this.lblRIStatus = new System.Windows.Forms.Label();
+            this.btnPortState = new System.Windows.Forms.Button();
+            this.cboParity = new System.Windows.Forms.ComboBox();
+            this.cboDataBits = new System.Windows.Forms.ComboBox();
+            this.btnHello = new System.Windows.Forms.Button();
+            this.rtbIncoming = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnGetSerialPorts
+            // 
+            this.btnGetSerialPorts.Location = new System.Drawing.Point(38, 95);
+            this.btnGetSerialPorts.Name = "btnGetSerialPorts";
+            this.btnGetSerialPorts.Size = new System.Drawing.Size(125, 33);
+            this.btnGetSerialPorts.TabIndex = 0;
+            this.btnGetSerialPorts.Text = "Ports";
+            this.btnGetSerialPorts.UseVisualStyleBackColor = true;
+            this.btnGetSerialPorts.Click += new System.EventHandler(this.btnGetSerialPorts_Click);
+            // 
+            // cboPorts
+            // 
+            this.cboPorts.FormattingEnabled = true;
+            this.cboPorts.Location = new System.Drawing.Point(192, 95);
+            this.cboPorts.Name = "cboPorts";
+            this.cboPorts.Size = new System.Drawing.Size(121, 21);
+            this.cboPorts.TabIndex = 2;
+            // 
+            // cboBaudRate
+            // 
+            this.cboBaudRate.FormattingEnabled = true;
+            this.cboBaudRate.Location = new System.Drawing.Point(192, 122);
+            this.cboBaudRate.Name = "cboBaudRate";
+            this.cboBaudRate.Size = new System.Drawing.Size(121, 21);
+            this.cboBaudRate.TabIndex = 3;
+            // 
+            // cboStopBits
+            // 
+            this.cboStopBits.FormattingEnabled = true;
+            this.cboStopBits.Location = new System.Drawing.Point(192, 153);
+            this.cboStopBits.Name = "cboStopBits";
+            this.cboStopBits.Size = new System.Drawing.Size(121, 21);
+            this.cboStopBits.TabIndex = 4;
+            // 
+            // cboHandShaking
+            // 
+            this.cboHandShaking.FormattingEnabled = true;
+            this.cboHandShaking.Location = new System.Drawing.Point(192, 180);
+            this.cboHandShaking.Name = "cboHandShaking";
+            this.cboHandShaking.Size = new System.Drawing.Size(121, 21);
+            this.cboHandShaking.TabIndex = 5;
+            // 
+            // lblBreakStatus
+            // 
+            this.lblBreakStatus.AutoSize = true;
+            this.lblBreakStatus.Location = new System.Drawing.Point(36, 309);
+            this.lblBreakStatus.Name = "lblBreakStatus";
+            this.lblBreakStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblBreakStatus.TabIndex = 6;
+            this.lblBreakStatus.Text = "Break";
+            // 
+            // lblCTSStatus
+            // 
+            this.lblCTSStatus.AutoSize = true;
+            this.lblCTSStatus.Location = new System.Drawing.Point(97, 309);
+            this.lblCTSStatus.Name = "lblCTSStatus";
+            this.lblCTSStatus.Size = new System.Drawing.Size(28, 13);
+            this.lblCTSStatus.TabIndex = 7;
+            this.lblCTSStatus.Text = "CTS";
+            // 
+            // lblDSRStatus
+            // 
+            this.lblDSRStatus.AutoSize = true;
+            this.lblDSRStatus.Location = new System.Drawing.Point(158, 309);
+            this.lblDSRStatus.Name = "lblDSRStatus";
+            this.lblDSRStatus.Size = new System.Drawing.Size(30, 13);
+            this.lblDSRStatus.TabIndex = 8;
+            this.lblDSRStatus.Text = "DSR";
+            // 
+            // lblRIStatus
+            // 
+            this.lblRIStatus.AutoSize = true;
+            this.lblRIStatus.Location = new System.Drawing.Point(219, 309);
+            this.lblRIStatus.Name = "lblRIStatus";
+            this.lblRIStatus.Size = new System.Drawing.Size(18, 13);
+            this.lblRIStatus.TabIndex = 9;
+            this.lblRIStatus.Text = "RI";
+            // 
+            // btnPortState
+            // 
+            this.btnPortState.Location = new System.Drawing.Point(38, 134);
+            this.btnPortState.Name = "btnPortState";
+            this.btnPortState.Size = new System.Drawing.Size(125, 30);
+            this.btnPortState.TabIndex = 11;
+            this.btnPortState.Text = "Closed";
+            this.btnPortState.UseVisualStyleBackColor = true;
+            this.btnPortState.Click += new System.EventHandler(this.btnPortState_Click);
+            // 
+            // cboParity
+            // 
+            this.cboParity.FormattingEnabled = true;
+            this.cboParity.Location = new System.Drawing.Point(192, 234);
+            this.cboParity.Name = "cboParity";
+            this.cboParity.Size = new System.Drawing.Size(121, 21);
+            this.cboParity.TabIndex = 12;
+            // 
+            // cboDataBits
+            // 
+            this.cboDataBits.FormattingEnabled = true;
+            this.cboDataBits.Location = new System.Drawing.Point(192, 207);
+            this.cboDataBits.Name = "cboDataBits";
+            this.cboDataBits.Size = new System.Drawing.Size(121, 21);
+            this.cboDataBits.TabIndex = 13;
+            // 
+            // btnHello
+            // 
+            this.btnHello.Location = new System.Drawing.Point(38, 170);
+            this.btnHello.Name = "btnHello";
+            this.btnHello.Size = new System.Drawing.Size(125, 30);
+            this.btnHello.TabIndex = 14;
+            this.btnHello.Text = "Hello";
+            this.btnHello.UseVisualStyleBackColor = true;
+            this.btnHello.Click += new System.EventHandler(this.btnHello_Click);
+            // 
+            // rtbIncoming
+            // 
+            this.rtbIncoming.Location = new System.Drawing.Point(371, 122);
+            this.rtbIncoming.Name = "rtbIncoming";
+            this.rtbIncoming.Size = new System.Drawing.Size(197, 131);
+            this.rtbIncoming.TabIndex = 15;
+            this.rtbIncoming.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 352);
+            this.Controls.Add(this.rtbIncoming);
+            this.Controls.Add(this.btnHello);
+            this.Controls.Add(this.cboDataBits);
+            this.Controls.Add(this.cboParity);
+            this.Controls.Add(this.btnPortState);
+            this.Controls.Add(this.lblRIStatus);
+            this.Controls.Add(this.lblDSRStatus);
+            this.Controls.Add(this.lblCTSStatus);
+            this.Controls.Add(this.lblBreakStatus);
+            this.Controls.Add(this.cboHandShaking);
+            this.Controls.Add(this.cboStopBits);
+            this.Controls.Add(this.cboBaudRate);
+            this.Controls.Add(this.cboPorts);
+            this.Controls.Add(this.btnGetSerialPorts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetSerialPorts;
+        private System.Windows.Forms.ComboBox cboPorts;
+        private System.Windows.Forms.ComboBox cboBaudRate;
+        private System.Windows.Forms.ComboBox cboStopBits;
+        private System.Windows.Forms.ComboBox cboHandShaking;
+        private System.Windows.Forms.Label lblBreakStatus;
+        private System.Windows.Forms.Label lblCTSStatus;
+        private System.Windows.Forms.Label lblDSRStatus;
+        private System.Windows.Forms.Label lblRIStatus;
+        private System.Windows.Forms.Button btnPortState;
+        private System.Windows.Forms.ComboBox cboParity;
+        private System.Windows.Forms.ComboBox cboDataBits;
+        private System.Windows.Forms.Button btnHello;
+        private System.Windows.Forms.RichTextBox rtbIncoming;
+    }
+}
+
---
+++ Prueba_RS232/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+
+namespace Prueba_RS232
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort comPort = new SerialPort();
+
+        internal delegate void SerialDataReceivedEventHandlerDelegate(
+                 object sender, SerialDataReceivedEventArgs e);
+
+        internal delegate void SerialPinChangedEventHandlerDelegate(
+                 object sender, SerialPinChangedEventArgs e);
+
+        private SerialPinChangedEventHandler SerialPinChangedEventHandler1;
+
+        delegate void SetTextCallback(string text);
+
+        string InputData = String.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SerialPinChangedEventHandler1 = new SerialPinChangedEventHandler(PinChanged);
+            comPort.DataReceived +=
+              new System.IO.Ports.SerialDataReceivedEventHandler(port_DataReceived_1);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGetSerialPorts_Click(object sender, EventArgs e)
+        {
+            string[] ArrayComPortsNames = null;
+            int index = -1;
+            string ComPortName = null;
+
+            //Com Ports
+            ArrayComPortsNames = SerialPort.GetPortNames();
+            do
+            {
+                index += 1;
+                cboPorts.Items.Add(ArrayComPortsNames[index]);
+            } while (!((ArrayComPortsNames[index] == ComPortName) ||
+              (index == ArrayComPortsNames.GetUpperBound(0))));
+            Array.Sort(ArrayComPortsNames);
+
+            if (index == ArrayComPortsNames.GetUpperBound(0))
+            {
+                ComPortName = ArrayComPortsNames[0];
+            }
+
+            //get first item print in text
+            cboPorts.Text = ArrayComPortsNames[0];
+
+            //Baud Rate
+            cboBaudRate.Items.Add(300);
+            cboBaudRate.Items.Add(600);
+            cboBaudRate.Items.Add(1200);
+            cboBaudRate.Items.Add(2400);
+            cboBaudRate.Items.Add(9600);
+            cboBaudRate.Items.Add(14400);
+            cboBaudRate.Items.Add(19200);
+            cboBaudRate.Items.Add(38400);
+            cboBaudRate.Items.Add(57600);
+            cboBaudRate.Items.Add(115200);
+            cboBaudRate.Items.ToString();
+
+            //get first item print in text
+            cboBaudRate.Text = cboBaudRate.Items[0].ToString();
+            //Data Bits
+            cboDataBits.Items.Add(7);
+            cboDataBits.Items.Add(8);
+            //get the first item print it in the text 
+            cboDataBits.Text = cboDataBits.Items[0].ToString();
+
+            //Stop Bits
+            cboStopBits.Items.Add("One");
+            cboStopBits.Items.Add("OnePointFive");
+            cboStopBits.Items.Add("Two");
+            //get the first item print in the text
+            cboStopBits.Text = cboStopBits.Items[0].ToString();
+
+            //Parity 
+            cboParity.Items.Add("None");
+            cboParity.Items.Add("Even");
+            cboParity.Items.Add("Mark");
+            cboParity.Items.Add("Odd");
+            cboParity.Items.Add("Space");
+
+            //get the first item print in the text
+
+            cboParity.Text = cboParity.Items[0].ToString();
+
+            //Handshake
+            cboHandShaking.Items.Add("None");
+            cboHandShaking.Items.Add("XOnXOff");
+            cboHandShaking.Items.Add("RequestToSend");
+            cboHandShaking.Items.Add("RequestToSendXOnXOff");
+
+            //get the first item print it in the text 
+            cboHandShaking.Text = cboHandShaking.Items[0].ToString();
+
+
+        }
+
+        private void port_DataReceived_1(object sender, SerialDataReceivedEventArgs e)
+        {
+            InputData = comPort.ReadExisting();
+            System.Diagnostics.Debug.WriteLine("Coño muestrame algo");
+            if (InputData != String.Empty)
+            {
+                this.BeginInvoke(new SetTextCallback(SetText), new object[] { InputData });
+            }
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            comPort.ReadLine();
+            System.Diagnostics.Debug.WriteLine("Coño muestrame algo");
+        }
+
+        private void SetText(string text)
+        {
+            this.rtbIncoming.Text += text;
+        }
+
+       /* private void btnTest_Click(object sender, EventArgs e)
+        {
+            SerialPinChangedEventHandler1 = new SerialPinChangedEventHandler(PinChanged);
+            comPort.PinChanged += SerialPinChangedEventHandler1;
+            try
+            {
+                comPort.Open();
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            comPort.RtsEnable = true;
+            comPort.DtrEnable = true;
+            btnTest.Enabled = false;
+        }*/
+
+        internal void PinChanged(object sender, SerialPinChangedEventArgs e)
+        {
+            SerialPinChange SerialPinChange1 = 0;
+            bool signalState = false;
+            SerialPinChange1 = e.EventType;
+            lblCTSStatus.BackColor = Color.Green;
+            lblDSRStatus.BackColor = Color.Green;
+            lblRIStatus.BackColor = Color.Green;
+            lblBreakStatus.BackColor = Color.Green;
+
+            switch (SerialPinChange1)
+            {
+                case SerialPinChange.Break:
+                    lblBreakStatus.BackColor = Color.Red;
+                    //MessageBox.Show("Break is Set");
+                    break;
+                case SerialPinChange.CDChanged:
+                    signalState = comPort.CtsHolding;
+                    //  MessageBox.Show("CD = " + signalState.ToString());
+                    break;
+                case SerialPinChange.CtsChanged:
+                    signalState = comPort.CDHolding;
+                    lblCTSStatus.BackColor = Color.Red;
+                    //MessageBox.Show("CTS = " + signalState.ToString());
+                    break;
+                case SerialPinChange.DsrChanged:
+                    signalState = comPort.DsrHolding;
+                    lblDSRStatus.BackColor = Color.Red;
+                    // MessageBox.Show("DSR = " + signalState.ToString());
+                    break;
+                case SerialPinChange.Ring:
+                    lblRIStatus.BackColor = Color.Red;
+                    //MessageBox.Show("Ring Detected");
+                    break;
+            }
+        }
+
+        private void btnPortState_Click(object sender, EventArgs e)
+        {
+            if (btnPortState.Text == "Closed")
+            {
+                btnPortState.Text = "Open";
+                comPort.PortName = Convert.ToString(cboPorts.Text);
+                comPort.BaudRate = Convert.ToInt32(cboBaudRate.Text);
+                comPort.DataBits = Convert.ToInt16(cboDataBits.Text);
+                comPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), cboStopBits.Text);
+                comPort.Handshake = (Handshake)Enum.Parse(typeof(Handshake), cboHandShaking.Text);
+                comPort.Parity = (Parity)Enum.Parse(typeof(Parity), cboParity.Text);
+                try
+                {
+                    comPort.Open();
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else if (btnPortState.Text == "Open")
+            {
+                btnPortState.Text = "Closed";
+                comPort.Close();
+            }
+        }
+
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                comPort.Write("Hello World!");
+            } catch(InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
