934ab88419e9353efa4bfec2c4913585d8498fd9
Ignacio Paradisi <ignacioparadisi@gmail.com>
2018-06-29 19:42:47 -0400

    La clase JailSquare fue creada

---
+++ Prueba_RS232/Logica/JailSquare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace Monopolio_RS232.Logica
+{
+    class JailSquare: Square
+    {
+        public JailSquare(String name): base(name)
+        {
+
+        }
+
+        public override void doAction(Player player, Board board)
+        {
+            Trace.WriteLine(player, player.getName() + " has been Jail and lost 500 money");
+            player.getMoney().substractMoney(500);
+        }
+    }
+}
---
+++ Prueba_RS232/Logica/VacationSquare.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopolio_RS232.Logica
+{
+    class VacationSquare
+    {
+    }
+}
