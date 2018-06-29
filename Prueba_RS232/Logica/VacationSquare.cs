8144b8d98ce2b5cc86edd477c624402f8ae9c5c5
Ignacio Paradisi <ignacioparadisi@gmail.com>
2018-06-29 19:51:15 -0400

    La clase VacationSquare fue creada

--- Prueba_RS232/Logica/VacationSquare.cs
+++ Prueba_RS232/Logica/VacationSquare.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
 
 namespace Monopolio_RS232.Logica
 {
-    class VacationSquare
+    class VacationSquare: Square
     {
+        public VacationSquare(String name): base(name)
+        {
+
+        }
+
+        public override void doAction(Player player, Board board)
+        {
+            Random rand = new Random();
+            Square square = board.movePlayer(player, rand.Next(board.getTotalSquare()), false);
+            Trace.WriteLine(player, player.getName() + " has go to vacation at " + square.getName());
+        }
     }
 }
