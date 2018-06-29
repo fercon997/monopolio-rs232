6acd3c86f5695b4f89650fea351644f083953840
Ignacio Paradisi <ignacioparadisi@gmail.com>
2018-06-29 17:54:11 -0400

    GoToJailSquare creada

---
+++ Prueba_RS232/Logica/GoToJailSquare.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace Monopolio_RS232.Logica
+{
+    class GoToJailSquare: Square
+    {
+        public GoToJailSquare(String name) : base(name)
+        {
+        }
+
+        public override void doAction(Player player, Board board)
+        {
+            Trace.WriteLine(player, player.getName() + "has go to Jail");
+            board.movePlayer(player, -board.getTotalSquare() / 2, false);
+        }
+    }
+}
